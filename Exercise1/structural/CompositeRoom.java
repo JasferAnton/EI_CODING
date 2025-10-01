4befa1185e19292a1435042f2a38ab04f4feae50
JasferAnton <158241422+JasferAnton@users.noreply.github.com>
2025-10-01 22:27:35 +0530

    Update CompositeRoom.java

--- Exercise1/structural/CompositeRoom.java
+++ Exercise1/structural/CompositeRoom.java
@@ -1,1 +1,21 @@
+package structural;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeRoom implements RoomComponent {
+    private String name;
+    private List<RoomComponent> rooms = new ArrayList<>();
+
+    public CompositeRoom(String name) { this.name = name; }
+
+    public void add(RoomComponent room) { rooms.add(room); }
+    public void remove(RoomComponent room) { rooms.remove(room); }
+
+    @Override
+    public void showStatus() {
+        System.out.println("Group: " + name);
+        for(RoomComponent r : rooms) r.showStatus();
+    }
+}
 

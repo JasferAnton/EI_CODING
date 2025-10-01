e08034c2a7efe5af1caa857f14c425e9f7f45a79
JasferAnton <158241422+JasferAnton@users.noreply.github.com>
2025-10-01 22:38:22 +0530

    Update BookRoomCommand.java

--- Exercise1/behavioral/command/BookRoomCommand.java
+++ Exercise1/behavioral/command/BookRoomCommand.java
@@ -1,1 +1,14 @@
+package behavioral.command;
+
+import behavioral.observer.Room;
+
+public class BookRoomCommand implements Command {
+    private Room room;
+    public BookRoomCommand(Room room) { this.room = room; }
+
+    @Override
+    public void execute() {
+        System.out.println("Booking room " + room.getRoomNumber());
+    }
+}
 

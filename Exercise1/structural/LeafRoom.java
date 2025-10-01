37237c6d83111f01492bf2e19a701eab5d0e3d65
JasferAnton <158241422+JasferAnton@users.noreply.github.com>
2025-10-01 22:29:26 +0530

    Update LeafRoom.java

--- Exercise1/structural/LeafRoom.java
+++ Exercise1/structural/LeafRoom.java
@@ -1,1 +1,14 @@
+package structural;
+
+import behavioral.observer.Room;
+
+public class LeafRoom implements RoomComponent {
+    private Room room;
+    public LeafRoom(Room room) { this.room = room; }
+
+    @Override
+    public void showStatus() {
+        System.out.println("Room " + room.getRoomNumber());
+    }
+}
 

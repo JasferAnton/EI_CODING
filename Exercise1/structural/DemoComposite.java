0a8b13a5827214337998d3de2e3359331dee2584
JasferAnton <158241422+JasferAnton@users.noreply.github.com>
2025-10-01 22:29:08 +0530

    Update DemoComposite.java

--- Exercise1/structural/DemoComposite.java
+++ Exercise1/structural/DemoComposite.java
@@ -1,1 +1,19 @@
+import structural.*;
+import behavioral.observer.Room;
+
+public class DemoComposite {
+    public static void main(String[] args) {
+        Room r1 = new Room(1);
+        Room r2 = new Room(2);
+
+        LeafRoom leaf1 = new LeafRoom(r1);
+        LeafRoom leaf2 = new LeafRoom(r2);
+
+        CompositeRoom group = new CompositeRoom("Conference Section");
+        group.add(leaf1);
+        group.add(leaf2);
+
+        group.showStatus();
+    }
+}
 

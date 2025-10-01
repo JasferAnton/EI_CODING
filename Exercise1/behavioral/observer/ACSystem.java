478ddb6803d2fe05e16ce8eba975966013175173
JasferAnton <158241422+JasferAnton@users.noreply.github.com>
2025-10-01 22:38:54 +0530

    Update ACSystem.java

--- Exercise1/behavioral/observer/ACSystem.java
+++ Exercise1/behavioral/observer/ACSystem.java
@@ -1,1 +1,9 @@
+package behavioral.observer;
+
+public class ACSystem implements Observer {
+    @Override
+    public void update(boolean occupied) {
+        System.out.println(occupied ? "AC ON" : "AC OFF");
+    }
+}
 
